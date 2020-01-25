68288219be8c1e47019fd2906cc78e3c4265d9ec
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-01-25 10:57:34 +0100

    Initial commit

---
+++ MyroWebApp.Xamarin/MyroWebApp.Xamarin/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MyroWebApp.Xamarin.Services;
+using MyroWebApp.Xamarin.Views;
+
+namespace MyroWebApp.Xamarin
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MyroWebApp.Xamarin/MyroWebApp.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyroWebApp.Xamarin.Models;
+
+namespace MyroWebApp.Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
