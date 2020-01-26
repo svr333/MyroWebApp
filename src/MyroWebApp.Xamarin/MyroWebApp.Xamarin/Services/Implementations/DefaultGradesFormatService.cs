fba129601626b99ed7aaed52916ff183e9b45760
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-01-26 14:01:22 +0100

    Add implementation of IGradesFormatService

---
+++ src/MyroWebApp.Xamarin/MyroWebApp.Xamarin/Services/Implementations/DefaultGradesFormatService.cs
@@ -0,0 +1,11 @@
+﻿using MyroWebApp.Xamarin.Entities.FormattedGrades;
+using MyroWebApp.Xamarin.Entities.Grades;
+
+namespace MyroWebApp.Xamarin.Services.Implementations
+{
+    public class DefaultGradesFormatService : IGradesFormatService
+    {
+        public FormattedGrades FormatGradesForXamarinLayout(Grades grades)
+            => new FormattedGrades(grades);
+    }
+}
