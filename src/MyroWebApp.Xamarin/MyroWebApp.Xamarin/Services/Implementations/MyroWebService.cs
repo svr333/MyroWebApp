5e5148378278610604b66b121756be2829d1b1c6
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-01-26 13:58:54 +0100

    Implement interface

--- src/MyroWebApp.Xamarin/MyroWebApp.Xamarin/Services/Implementations/MyroWebService.cs
+++ src/MyroWebApp.Xamarin/MyroWebApp.Xamarin/Services/Implementations/MyroWebService.cs
@@ -1,10 +1,17 @@
-﻿using MyroWebApp.Xamarin.Entities.Grades;
+﻿using MyroWebApp.Xamarin.Entities;
+using MyroWebApp.Xamarin.Entities.FormattedGrades;
+using MyroWebApp.Xamarin.Entities.Grades;
 using System;
 
 namespace MyroWebApp.Xamarin.Services.Implementations
 {
     public class MyroWebService : IMyroWebService
     {
+        public FormattedGrades GetUserFormattedGrades(string key)
+        {
+            throw new NotImplementedException();
+        }
+
         public Grades GetUserGrades(string key)
         {
             throw new NotImplementedException();
